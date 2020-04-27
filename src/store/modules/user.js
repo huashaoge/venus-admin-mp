c1385f8824f826c18f44518fc54b34536b2995f4
tcg <546103589@qq.com>
2020-04-27 17:26:12 +0800

    调整后端返回值

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -35,8 +35,8 @@ const actions = {
     return new Promise((resolve, reject) => {
       login({ username: username.trim(), password: password }).then(response => {
         const { data } = response
-        commit('SET_TOKEN', data.token)
-        setToken(data.token)
+        commit('SET_TOKEN', data.access_token)
+        setToken(data.access_token)
         resolve()
       }).catch(error => {
         reject(error)
